ac3d15aac743aa2d6b2827921baea14f84adf171
Your Name <you@example.com>
2025-08-26 11:57:51 +0530

    payment mode update

--- models/refundEnquiry.js
+++ models/refundEnquiry.js
@@ -22,7 +22,7 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false
         },
         PAYMENT_MODE: {
-            type: DataTypes.ENUM("twint", "bank"),
+            type: DataTypes.ENUM("twint", "bank", "card"),
             allowNull: true
         },
         TWINT_ACCOUNT: {
